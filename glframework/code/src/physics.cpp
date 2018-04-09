719ef76ba555067c51368bb4eace8e08fb1b1919
Guillem Serra <guillemserralorenz@enti.cat>
2018-04-09 16:16:23 +0200

    cube and gui start

--- glframework/code/src/physics.cpp
+++ glframework/code/src/physics.cpp
@@ -1,38 +1,113 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+//#include <glm\gtc\type_ptr.hpp>
+#include <iostream>
+#include <time.h>
+#include <SDL.h>
+
+glm::mat4 randomCubeTransform;
+
+namespace Cube
+{
+	extern void setupCube();
+	extern void cleanupCube();
+	extern void updateCube(const glm::mat4& transform);
+	extern void drawCube();
+}
+void PhysicsInit();
+
+float randomFloat(float min, float max)
+{
+	return ((max - min) * ((float)rand() / RAND_MAX)) + min;
+}
+
+#pragma region GUI Variables
+static bool playSimulation = true;
+int clicked = 0;
+float totalResetTime = 15.0f;
+float resetTime;
+glm::vec3 gravityAccel = { 0.0f,-9.81,0.0f };
+
+bool useCollisions = true;
+float elasticCoefficient = 0.2f;
+float frictionCoefficient = 0.1f;
+#pragma endregion
 
 bool show_test_window = false;
-void GUI() {
+void GUI()
+{
 	bool show = true;
 	ImGui::Begin("Physics Parameters", &show, 0);
 
-	// Do your GUI code here....
-	{	
+	{
+		//ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
-		
+		ImGui::Checkbox("Play simulation", &playSimulation);
+		if (ImGui::Button("Reset Simulation"))
+		{
+			clicked++;
+		}
+		if (clicked & 1)
+		{
+			PhysicsInit();
+			clicked--;
+		}
+		ImGui::DragFloat("Reset Time", &totalResetTime, 0.05f);
+		ImGui::InputFloat3("Gravity Accel", (float*)&gravityAccel);
+
+		if (ImGui::TreeNode("Collisions"))
+		{
+			ImGui::Checkbox("Use Collisions", &useCollisions);
+			ImGui::DragFloat("Elastic Coefficient", &elasticCoefficient, 0.005f);
+			ImGui::DragFloat("Friction Coefficient", &frictionCoefficient, 0.005f);
+
+			ImGui::TreePop();
+		}
 	}
 	// .........................
-	
+
 	ImGui::End();
 
-	// Example code -- ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
-	if(show_test_window) {
+	if (show_test_window)
+	{
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
 		ImGui::ShowTestWindow(&show_test_window);
 	}
 }
 
-void PhysicsInit() {
-	// Do your initialization code here...
-	// ...................................
+void PhysicsInit()
+{
+	//Time:
+	resetTime = 0.0f;
+	//Random:
+	srand(static_cast<unsigned int>(_getpid()) ^ static_cast<unsigned int>(clock()) ^ static_cast<unsigned int>(time(NULL)));
+
+	randomCubeTransform = glm::translate(glm::mat4(), glm::vec3(randomFloat(-5.0f,5.0f), randomFloat(0.0f, 10.0f), randomFloat(-5.0f, 5.0f)));
+	randomCubeTransform *= glm::rotate(glm::mat4(), randomFloat(0.0f, 180.f), glm::vec3(rand()%2, rand()%2, rand()%2));
+
+	//Cube::setupCube();
+	Cube::updateCube(glm::mat4());
 }
 
-void PhysicsUpdate(float dt) {
-	// Do your update code here...
-	// ...........................
+void PhysicsUpdate(float dt)
+{
+	if (playSimulation)
+	{
+		if (resetTime >= totalResetTime)
+		{
+			clicked++;
+		}
+		else
+		{
+			resetTime += dt;
+			Cube::drawCube();
+		}
+	}
 }
 
-void PhysicsCleanup() {
-	// Do your cleanup code here...
-	// ............................
+void PhysicsCleanup()
+{
+	Cube::cleanupCube();
 }
